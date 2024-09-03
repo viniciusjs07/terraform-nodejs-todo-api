60621f05b3be10ec3df0c982f6d4d6d0b63c913f
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2024-09-02 21:25:04 -0300

    create output.tfs in network

---
+++ terraform/01-network/outputs.tf
@@ -0,0 +1,41 @@
+output "vpc" {
+  value = {
+    id = aws_vpc.this.id
+    arn = aws_vpc.this.arn
+    cidr_block = aws_vpc.this.cidr_block
+  }
+}
+
+output "subnets" {
+    value = {
+        private = {
+            id = aws_subnet.private.*.id 
+            cidr_block = aws_subnet.private.*.cidr_block
+        }
+        public = {
+             id = aws_subnet.public.*.id 
+             cidr_block = aws_subnet.public.*.cidr_block
+        }
+    }
+  
+}
+
+output "route_tables" {
+    value = {
+        private = {
+            id = aws_route_table.private.*.id 
+        }
+        public = {
+            id = aws_route_table.public.*.id 
+        }
+    }
+  
+}
+
+output "az_count" {
+  value = var.network.az_count
+}
+
+output "selected_availability_zones" {
+  value = local.selected_availability_zones
+}
